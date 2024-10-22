ed742bbee6faa4227e90ed4401ed3dbde2d4fcb0
9090 <122942169+9090H9091@users.noreply.github.com>
2024-10-22 09:23:31 -0400

    Update rate_limit.py

--- helpers/rate_limit.py
+++ helpers/rate_limit.py
@@ -4,8 +4,10 @@ from helpers.logger import get_logger
 
 logger = get_logger(__name__)
 
+    # Limits users from spamming the bot, Will impliment ban/mute feature later
+    
 class RateLimiter:
-    def __init__(self, cooldown=5):  # 5 seconds default cooldown
+    def __init__(self, cooldown=3):  # 3 seconds default cooldown
         self.cooldown = cooldown
         self.last_used = defaultdict(float)
 
